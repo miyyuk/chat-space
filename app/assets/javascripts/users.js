f42c7cfc09b1c8e3fe65b23988bb79d0bdbf125d
miyyuk <orion.my.2@icloud.com>
2020-04-28 21:31:02 +0900

    edit-display_group_users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,7 +11,7 @@ $(function () {
   }
 
   function addNoUser() {
-    var html = `
+    let html = `
               <div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">ユーザーが見つかりません</p>
               </div>
@@ -19,8 +19,24 @@ $(function () {
     $("#user-search-result").append(html);
   }
 
+  function addDeleteUser(name, id) {
+    let html = `
+                <div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>
+                `
+    $(".js-add-user").append(html);
+  }
+
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+
   $("#user-search-field").on("keyup", function () {
-    var input = $("#user-search-field").val();
+    let input = $("#user-search-field").val();
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -44,4 +60,20 @@ $(function () {
         alert("ユーザー検索に失敗しました");
       })
   })
-})
+
+  $(document).on('click', ".chat-group-user__btn--add", function () {
+    const userName = $(this).attr("data-user-name")
+    const userId = $(this).attr("data-user-id")
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on('click', ".chat-group-user__btn--remove", function () {
+    $(this)
+      .parent()
+      .remove();
+  });
+
+});
